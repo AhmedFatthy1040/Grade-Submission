17221e49c1e889573357ef63fd8045ac5b54838d
AhmedFatthy1040 <91102592+AhmedFatthy1040@users.noreply.github.com>
2024-03-20 01:51:28 +0200

    Initialized the project.

---
+++ src/main/java/com/lorax/gradesubmission/GradeSubmissionApplication.java
@@ -0,0 +1,13 @@
+package com.lorax.gradesubmission;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradeSubmissionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradeSubmissionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lorax/gradesubmission/GradeSubmissionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lorax.gradesubmission;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradeSubmissionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
