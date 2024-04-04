7c36d47e0e58f82e2ec56be01478354386d08404
AhmedFatthy1040 <91102592+AhmedFatthy1040@users.noreply.github.com>
2024-04-04 12:25:52 +0200

    Added the update button logic to the backend.

--- src/main/java/com/lorax/gradesubmission/GradeController.java
+++ src/main/java/com/lorax/gradesubmission/GradeController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
@@ -15,14 +16,21 @@ public class GradeController {
     List<Grade> studentGrades = new ArrayList<>();
 
     @GetMapping("/")
-    public String gradeForm(Model model) {
-        model.addAttribute("grade", new Grade());
+    public String gradeForm(Model model, @RequestParam(required = false) String name) {
+        Integer gradeIndex = getGradeIndex(name);
+        model.addAttribute("grade", gradeIndex == -1 ? new Grade() : studentGrades.get(gradeIndex));
         return "form";
     }
 
     @PostMapping("/handleSubmit")
     public String submitFrom(Grade grade) {
-        studentGrades.add(grade);
+        Integer gradeIndex = getGradeIndex(grade.getName());
+        if (gradeIndex == -1) {
+            studentGrades.add(grade);
+        } else {
+            studentGrades.set(gradeIndex, grade);
+        }
+
         return "redirect:/grades";
     }
 
@@ -30,5 +38,14 @@ public class GradeController {
     public String getGrades(Model model) {
         model.addAttribute("grades", studentGrades);
         return "grades";
+    }
+
+    public Integer getGradeIndex(String name) {
+        for (int i = 0; i < studentGrades.size(); i++) {
+            if (studentGrades.get(i).getName().equals(name)) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
