2511f6ccd0c1b94e1ecdd8248da6b8cb4b1eef3b
AhmedFatthy1040 <91102592+AhmedFatthy1040@users.noreply.github.com>
2024-04-23 00:04:44 +0200

    Created a repository class to communicate with the datastore.

---
+++ src/main/java/com/lorax/gradesubmission/repository/GradeRepository.java
@@ -0,0 +1,26 @@
+package com.lorax.gradesubmission.repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.lorax.gradesubmission.Grade;
+
+public class GradeRepository {
+    private List<Grade> studentGrades = new ArrayList<>();
+
+    public Grade getGrade(int index) {
+        return studentGrades.get(index);
+    }
+
+    public void addGrade(Grade grade) {
+        studentGrades.add(grade);
+    }
+
+    public void updateGrade(Grade grade, int index) {
+        studentGrades.set(index, grade);
+    }
+
+    public List<Grade> getGrades() {
+        return studentGrades;
+    }
+}
