fe21c17d1f5c720b91e03278153f62ea97bb91b2
AhmedFatthy1040 <91102592+AhmedFatthy1040@users.noreply.github.com>
2024-04-23 00:03:11 +0200

    Stripped the controller class of CRUD related operations.

---
+++ src/main/java/com/lorax/gradesubmission/controller/GradeController.java
@@ -0,0 +1,44 @@
+package com.lorax.gradesubmission.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.lorax.gradesubmission.Grade;
+import com.lorax.gradesubmission.service.GradeService;
+
+import jakarta.validation.Valid;
+
+
+@Controller
+public class GradeController {
+
+    GradeService gradeService = new GradeService();
+
+    @GetMapping("/")
+    public String gradeForm(Model model, @RequestParam(required = false) String id) {
+        model.addAttribute("grade", gradeService.getGradeById(id));
+        return "form";
+    }
+
+    @PostMapping("/handleSubmit")
+    public String submitFrom(@Valid Grade grade, BindingResult result) {
+        if (result.hasErrors()) {
+            return "form";
+        }
+
+        gradeService.submitGrade(grade);
+
+        return "redirect:/grades";
+    }
+
+    @GetMapping("/grades")
+    public String getGrades(Model model) {
+        model.addAttribute("grades", gradeService.getGrades());
+        return "grades";
+    }
+}
