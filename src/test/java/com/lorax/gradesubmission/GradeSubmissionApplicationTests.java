588e0f5f3d0686eba894e0da789eeba5f6111062
AhmedFatthy1040 <91102592+AhmedFatthy1040@users.noreply.github.com>
2024-04-24 00:23:31 +0200

    Performed a mock get request on the form page.

--- src/test/java/com/lorax/gradesubmission/GradeSubmissionApplicationTests.java
+++ src/test/java/com/lorax/gradesubmission/GradeSubmissionApplicationTests.java
@@ -1,12 +1,15 @@
 package com.lorax.gradesubmission;
 
 import static org.junit.Assert.assertNotNull;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -18,6 +21,16 @@ class GradeSubmissionApplicationTests {
 	@Test
 	void contextLoads() {
 		assertNotNull(mockMvc);
+	}
+
+	@Test
+	public void testShowGradeForm() throws Exception {
+		RequestBuilder request = MockMvcRequestBuilders.get("/?id=123");
+
+		mockMvc.perform(request)
+		.andExpect(status().is2xxSuccessful())
+		.andExpect(view().name("form"))
+		.andExpect(model().attributeExists("grade"));
 	}
 
 }
