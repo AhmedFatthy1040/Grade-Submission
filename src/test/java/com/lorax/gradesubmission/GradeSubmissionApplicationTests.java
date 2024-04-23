d75cec06789d74ca1488d2eaead96d1f433cecb6
AhmedFatthy1040 <91102592+AhmedFatthy1040@users.noreply.github.com>
2024-04-24 00:12:54 +0200

    Set up a class for integration testing.

--- src/test/java/com/lorax/gradesubmission/GradeSubmissionApplicationTests.java
+++ src/test/java/com/lorax/gradesubmission/GradeSubmissionApplicationTests.java
@@ -1,13 +1,23 @@
 package com.lorax.gradesubmission;
 
+import static org.junit.Assert.assertNotNull;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class GradeSubmissionApplicationTests {
+
+	@Autowired
+	private MockMvc mockMvc;
 
 	@Test
 	void contextLoads() {
+		assertNotNull(mockMvc);
 	}
 
 }
