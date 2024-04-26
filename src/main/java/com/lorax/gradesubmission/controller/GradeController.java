9904f503e32cacbf959b04e89cb5ad97e8311790
AhmedFatthy1040 <91102592+AhmedFatthy1040@users.noreply.github.com>
2024-04-27 00:31:56 +0300

    Updated the grade controller to be a REST Controller.

--- src/main/java/com/lorax/gradesubmission/controller/GradeController.java
+++ src/main/java/com/lorax/gradesubmission/controller/GradeController.java
@@ -1,46 +1,65 @@
 package com.lorax.gradesubmission.controller;
 
+import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import com.lorax.gradesubmission.Grade;
+import com.lorax.gradesubmission.model.Grade;
 import com.lorax.gradesubmission.service.GradeService;
 
-import jakarta.validation.Valid;
-
-
-@Controller
+@RestController
+@RequestMapping("/grade")
 public class GradeController {
 
-    @Autowired
     GradeService gradeService;
 
-    @GetMapping("/")
-    public String gradeForm(Model model, @RequestParam(required = false) String id) {
-        model.addAttribute("grade", gradeService.getGradeById(id));
-        return "form";
+    public GradeController(GradeService gradeService) {
+        this.gradeService = gradeService;
     }
 
-    @PostMapping("/handleSubmit")
-    public String submitFrom(@Valid Grade grade, BindingResult result) {
-        if (result.hasErrors()) {
-            return "form";
-        }
-
-        gradeService.submitGrade(grade);
-
-        return "redirect:/grades";
+    @GetMapping("/student/{studentId}/course/{courseId}")
+    public ResponseEntity<Grade> getGrade(@PathVariable Long studentId, @PathVariable Long courseId) {
+        return new ResponseEntity<>(gradeService.getGrade(studentId, courseId), HttpStatus.OK);
     }
 
-    @GetMapping("/grades")
-    public String getGrades(Model model) {
-        model.addAttribute("grades", gradeService.getGrades());
-        return "grades";
+    @PostMapping("/student/{studentId}/course/{courseId}")
+    public ResponseEntity<Grade> saveGrade(@RequestBody Grade grade, @PathVariable Long studentId, @PathVariable Long courseId) {
+        return new ResponseEntity<>(gradeService.saveGrade(grade, studentId, courseId), HttpStatus.CREATED);
     }
+
+    @PutMapping("/student/{studentId}/course/{courseId}")
+    public ResponseEntity<Grade> updateGrade(@RequestBody Grade grade, @PathVariable Long studentId, @PathVariable Long courseId) {
+        return new ResponseEntity<>(gradeService.updateGrade(grade.getScore(), studentId, courseId), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/student/{studentId}/course/{courseId}")
+    public ResponseEntity<HttpStatus> deleteGrade(@PathVariable Long studentId, @PathVariable Long courseId) {
+        gradeService.deleteGrade(studentId, courseId);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @GetMapping("/student/{studentId}")
+    public ResponseEntity<List<Grade>> getStudentGrades(@PathVariable Long studentId) {
+        return new ResponseEntity<>(gradeService.getStudentGrades(studentId), HttpStatus.OK);
+    }
+
+    @GetMapping("/course/{courseId}")
+    public ResponseEntity<List<Grade>> getCourseGrades(@PathVariable Long courseId) {
+        return new ResponseEntity<>(gradeService.getCourseGrades(courseId), HttpStatus.OK);
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<Grade>> getGrades() {
+        return new ResponseEntity<>(gradeService.getAllGrades(), HttpStatus.OK);
+    }
+
 }
