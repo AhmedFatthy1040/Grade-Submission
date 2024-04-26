b22b931f0a74be566c470e404adb125984547b7d
AhmedFatthy1040 <91102592+AhmedFatthy1040@users.noreply.github.com>
2024-04-27 00:34:22 +0300

    Made the student model.

---
+++ src/main/java/com/lorax/gradesubmission/model/Student.java
@@ -0,0 +1,43 @@
+package com.lorax.gradesubmission.model;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@RequiredArgsConstructor
+@NoArgsConstructor
+@Table(name = "student")
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @NonNull
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @NonNull
+    @Column(name = "birth_date", nullable = false)
+    private LocalDate birthDate;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "student", cascade = CascadeType.ALL)
+    private List<Grade> grades;
+
+}
