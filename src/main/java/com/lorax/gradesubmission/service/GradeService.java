bb091e24ae4001c6957daffd2573b59ab87badc9
AhmedFatthy1040 <91102592+AhmedFatthy1040@users.noreply.github.com>
2024-04-27 00:37:25 +0300

    Created the grade service interface.

--- src/main/java/com/lorax/gradesubmission/service/GradeService.java
+++ src/main/java/com/lorax/gradesubmission/service/GradeService.java
@@ -2,55 +2,14 @@ package com.lorax.gradesubmission.service;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import com.lorax.gradesubmission.model.Grade;
 
-import com.lorax.gradesubmission.Constants;
-import com.lorax.gradesubmission.Grade;
-import com.lorax.gradesubmission.repository.GradeRepository;
-
-@Service
-public class GradeService {
-
-    @Autowired
-    GradeRepository gradeRepository;
-
-    public Grade getGrade(int index) {
-        return gradeRepository.getGrade(index);
-    }
-
-    public void addGrade(Grade grade) {
-        gradeRepository.addGrade(grade);
-    }
-
-    public void updateGrade(Grade grade, int index) {
-        gradeRepository.updateGrade(grade, index);
-    }
-
-    public List<Grade> getGrades() {
-        return gradeRepository.getGrades();
-    }
-
-    public int getGradeIndex(String id) {
-        for (int i = 0; i < getGrades().size(); i++) {
-            if (getGrade(i).getId().equals(id)) {
-                return i;
-            }
-        }
-        return Constants.NOT_FOUND;
-    }
-
-    public Grade getGradeById(String id) {
-        int index = getGradeIndex(id);
-        return index == Constants.NOT_FOUND ? new Grade() : getGrade(index);
-    }
-
-    public void submitGrade(Grade grade) {
-        int index = getGradeIndex(grade.getId());
-        if (index == Constants.NOT_FOUND) {
-            addGrade(grade);
-        } else {
-            updateGrade(grade, index);
-        }
-    }
+public interface GradeService {
+    Grade getGrade(Long studentId, Long courseId);
+    Grade saveGrade(Grade grade, Long studentId, Long courseId);
+    Grade updateGrade(String score, Long studentId, Long courseId);
+    void deleteGrade(Long studentId, Long courseId);
+    List<Grade> getStudentGrades(Long studentId);
+    List<Grade> getCourseGrades(Long courseId);
+    List<Grade> getAllGrades();
 }
