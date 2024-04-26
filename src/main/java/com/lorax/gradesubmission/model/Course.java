c89c5f2d6c7fa1bf26d32ff0f785dc52e773f0de
AhmedFatthy1040 <91102592+AhmedFatthy1040@users.noreply.github.com>
2024-04-27 00:34:11 +0300

    Created the course model.

---
+++ src/main/java/com/lorax/gradesubmission/model/Course.java
@@ -0,0 +1,45 @@
+package com.lorax.gradesubmission.model;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@RequiredArgsConstructor
+@NoArgsConstructor
+@Table(name = "course")
+public class Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @NonNull
+    @Column(name = "subject", nullable = false)
+    private String subject;
+
+    @NonNull
+    @Column(name = "code", nullable = false, unique = true)
+    private String code;
+
+    @NonNull
+    @Column(name = "description", nullable = false)
+    private String description;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "course", cascade = CascadeType.ALL)
+    private List<Grade> grades;
+
+}
