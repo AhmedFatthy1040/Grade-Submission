ff2a50a924c8e6c12eee4d364fa9e6f5c91c6109
AhmedFatthy1040 <91102592+AhmedFatthy1040@users.noreply.github.com>
2024-04-27 00:38:09 +0300

    Implemented the course service with the business logic.

---
+++ src/main/java/com/lorax/gradesubmission/service/CourseServiceImpl.java
@@ -0,0 +1,46 @@
+package com.lorax.gradesubmission.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.lorax.gradesubmission.exception.CourseNotFoundException;
+import com.lorax.gradesubmission.model.Course;
+import com.lorax.gradesubmission.repository.CourseRepository;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+@Service
+public class CourseServiceImpl implements CourseService {
+
+    CourseRepository courseRepository;
+
+    @Override
+    public Course getCourse(Long id) {
+        Optional<Course> course = courseRepository.findById(id);
+        return unwrapCourse(course, id);
+    }
+
+    @Override
+    public Course saveCourse(Course course) {
+        return courseRepository.save(course);
+    }
+
+    @Override
+    public void deleteCourse(Long id) {
+        courseRepository.deleteById(id);
+    }
+
+    @Override
+    public List<Course> getCourses() {
+        return (List<Course>) courseRepository.findAll();
+    }
+
+    static Course unwrapCourse(Optional<Course> entity, Long id) {
+        if (entity.isPresent()) return entity.get();
+        else throw new CourseNotFoundException(id);
+    }
+
+}
