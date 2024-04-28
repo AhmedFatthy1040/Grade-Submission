b76efff96996a3ba46bd0f504d4ee77f81fe3af9
AhmedFatthy1040 <91102592+AhmedFatthy1040@users.noreply.github.com>
2024-04-29 01:44:28 +0300

    Removed the course not found exception, and used entity not found exception instead of it.

--- src/main/java/com/lorax/gradesubmission/service/CourseServiceImpl.java
+++ src/main/java/com/lorax/gradesubmission/service/CourseServiceImpl.java
@@ -5,7 +5,6 @@ import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
-import com.lorax.gradesubmission.exception.CourseNotFoundException;
 import com.lorax.gradesubmission.exception.EntityNotFoundException;
 import com.lorax.gradesubmission.model.Course;
 import com.lorax.gradesubmission.repository.CourseRepository;
