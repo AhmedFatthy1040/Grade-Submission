945635bba6c7bf40d2b392780d1d57c494c27e66
AhmedFatthy1040 <91102592+AhmedFatthy1040@users.noreply.github.com>
2024-04-28 22:52:30 +0300

    Allowed all requests for the register path

---
+++ src/main/java/com/lorax/gradesubmission/security/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.lorax.gradesubmission.security;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import lombok.AllArgsConstructor;
+
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+
+@Configuration
+@AllArgsConstructor
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf().disable()
+            .authorizeRequests()
+            .requestMatchers(HttpMethod.POST, SecurityConstants.REGISTER_PATH).permitAll()
+            .anyRequest().authenticated()
+            .and()
+            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        return http.build();
+    }
+
+}
