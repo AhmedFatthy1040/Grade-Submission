c7bd46dc585ebd286688639e79497b42a7d89610
AhmedFatthy1040 <91102592+AhmedFatthy1040@users.noreply.github.com>
2024-04-29 01:45:59 +0300

    Removed the student not found exception, and used entity not found exception instead of it.

--- src/main/java/com/lorax/gradesubmission/exception/StudentNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.lorax.gradesubmission.exception;
-
-public class StudentNotFoundException extends RuntimeException { 
-
-    public StudentNotFoundException(Long id) {
-        super("The student id '" + id + "' does not exist in our records");
-    }
-
-}
