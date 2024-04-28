d42310b566f4fa405a57d3dda4058a392fec5336
AhmedFatthy1040 <91102592+AhmedFatthy1040@users.noreply.github.com>
2024-04-28 22:40:10 +0300

    Made the user REST controller.

---
+++ src/main/java/com/lorax/gradesubmission/controller/UserController.java
@@ -0,0 +1,37 @@
+package com.lorax.gradesubmission.controller;
+
+import jakarta.validation.Valid;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.lorax.gradesubmission.model.User;
+import com.lorax.gradesubmission.service.UserService;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+
+    UserService userService;
+
+	@GetMapping("/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id) {
+		return new ResponseEntity<>(HttpStatus.OK);
+	}
+
+    @PostMapping("/register")
+	public ResponseEntity<User> createUser(@Valid @RequestBody User user) {
+		return new ResponseEntity<>(HttpStatus.CREATED);
+	}
+
+}
