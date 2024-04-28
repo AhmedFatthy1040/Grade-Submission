29ddcccc0fa9748d8e581691f0dccba691943cf9
AhmedFatthy1040 <91102592+AhmedFatthy1040@users.noreply.github.com>
2024-04-28 22:42:52 +0300

    Updated the logic to use the entity not found exception.

--- src/main/java/com/lorax/gradesubmission/service/CourseServiceImpl.java
+++ src/main/java/com/lorax/gradesubmission/service/CourseServiceImpl.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.stereotype.Service;
 
 import com.lorax.gradesubmission.exception.CourseNotFoundException;
+import com.lorax.gradesubmission.exception.EntityNotFoundException;
 import com.lorax.gradesubmission.model.Course;
 import com.lorax.gradesubmission.repository.CourseRepository;
 
@@ -40,7 +41,7 @@ public class CourseServiceImpl implements CourseService {
 
     static Course unwrapCourse(Optional<Course> entity, Long id) {
         if (entity.isPresent()) return entity.get();
-        else throw new CourseNotFoundException(id);
+        else throw new EntityNotFoundException(id, Course.class);
     }
 
 }
