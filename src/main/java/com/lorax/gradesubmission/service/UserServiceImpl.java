0de41da329cabcc910938bfe90a12ce3e17a392f
AhmedFatthy1040 <91102592+AhmedFatthy1040@users.noreply.github.com>
2024-04-28 22:45:08 +0300

    Implemented the user service to hold the business logic.

---
+++ src/main/java/com/lorax/gradesubmission/service/UserServiceImpl.java
@@ -0,0 +1,36 @@
+package com.lorax.gradesubmission.service;
+
+import java.util.Optional;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.lorax.gradesubmission.model.User;
+import com.lorax.gradesubmission.exception.EntityNotFoundException;
+import com.lorax.gradesubmission.repository.UserRepository;
+
+import lombok.AllArgsConstructor;
+@Service
+@AllArgsConstructor
+public class UserServiceImpl implements UserService {
+
+    private UserRepository userRepository;
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Override
+    public User getUser(Long id) {
+        Optional<User> user = userRepository.findById(id);
+        return unwrapUser(user, id);
+    }
+
+    @Override
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+
+    static User unwrapUser(Optional<User> entity, Long id) {
+        if (entity.isPresent()) return entity.get();
+        else throw new EntityNotFoundException(id, User.class);
+    }
+
+}
