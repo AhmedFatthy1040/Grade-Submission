f47a5974ad94698d5f62abdc45b260fa9ac11663
AhmedFatthy1040 <91102592+AhmedFatthy1040@users.noreply.github.com>
2024-04-29 21:26:34 +0300

    Used the optional object to make the code more cleaner.

--- src/main/java/com/lorax/gradesubmission/repository/UserRepository.java
+++ src/main/java/com/lorax/gradesubmission/repository/UserRepository.java
@@ -1,9 +1,11 @@
 package com.lorax.gradesubmission.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.lorax.gradesubmission.model.User;
 
 public interface UserRepository extends CrudRepository<User, Long> {
-	User findByUsername(String username);
+	Optional<User> findByUsername(String username);
 }
