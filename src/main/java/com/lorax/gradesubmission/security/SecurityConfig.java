c88f77d8834462e00711883eead3d4fe140f2b4f
AhmedFatthy1040 <91102592+AhmedFatthy1040@users.noreply.github.com>
2024-04-29 21:27:36 +0300

    Added 3 pathways to the filter chain (register, login, and token authorization)

--- src/main/java/com/lorax/gradesubmission/security/SecurityConfig.java
+++ src/main/java/com/lorax/gradesubmission/security/SecurityConfig.java
@@ -6,31 +6,39 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+
+import com.lorax.gradesubmission.security.filter.AuthenticationFilter;
+import com.lorax.gradesubmission.security.filter.ExceptionHandlerFilter;
+import com.lorax.gradesubmission.security.filter.JWTAuthorizationFilter;
+import com.lorax.gradesubmission.security.manager.CustomAuthenticationManager;
+
 import lombok.AllArgsConstructor;
 
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 
 @Configuration
 @AllArgsConstructor
 public class SecurityConfig {
 
+    CustomAuthenticationManager customAuthenticationManager;
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        AuthenticationFilter authenticationFilter = new AuthenticationFilter(customAuthenticationManager);
+        authenticationFilter.setFilterProcessesUrl("/authenticate");
         http
             .csrf().disable()
             .authorizeRequests()
             .requestMatchers(HttpMethod.POST, SecurityConstants.REGISTER_PATH).permitAll()
             .anyRequest().authenticated()
             .and()
+            .addFilterBefore(new ExceptionHandlerFilter(), AuthenticationFilter.class)
+            .addFilter(authenticationFilter)
+            .addFilterAfter(new JWTAuthorizationFilter(), AuthenticationFilter.class)
             .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         return http.build();
-    }
-
-    @Bean
-    public BCryptPasswordEncoder bCryptPasswordEncoder() {
-        return new BCryptPasswordEncoder();
     }
 
 }
